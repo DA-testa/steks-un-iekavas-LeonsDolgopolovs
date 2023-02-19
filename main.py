a02ceb10ed62e42d9ae46537f6b4ddd04a086558
LeonsDolgopolovs <125027263+LeonsDolgopolovs@users.noreply.github.com>
2023-02-20 01:02:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 # python3
 
-from collections import namedtuple 
- 
-Bracket = namedtuple("Bracket", ["char", "position"]) 
- 
- 
-def are_matching(left, right): 
-    return (left + right) in ["()", "[]", "{}"] 
- 
- 
-def find_mismatch(text): 
-    opening_brackets_stack = [] 
-    for i, next in enumerate(text): 
-        if next in "([{": 
-            opening_brackets_stack.append(Bracket(next, i+1)) 
-        if next in ")]}": 
-            if not opening_brackets_stack: 
-                return i+1 
-            if not are_matching(opening_brackets_stack[-1].char, next): 
-                return i+1 
-            opening_brackets_stack.pop() 
-    if opening_brackets_stack: 
-        return opening_brackets_stack[-1].position 
-    else: 
-        return "Success" 
- 
- 
-def main(): 
-    choice = input("Use an input to choice files or input - F or I:") 
-    if choice == "F": 
-        filename = input("Enter file name:") 
-        with open(filename, "r") as f: 
-            text = f.readline().strip() 
-    else: 
-        text = input("Enter the brackets:") 
-    mismatch = find_mismatch(text) 
-    print(mismatch) 
- 
- 
-if name == "__main__": 
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i+1))
+            pass
+
+        if next in ")]}":
+            if not opening_brackets_stack:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
+            pass
+        
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
+        returns "Success"
+            
+def main():
+    text = input("F or I")
+    if "F" in text:
+        name input("Enter gile name: ")
+        with open(name, "r", enconding="latinl") as file:
+            text1=file.read()
+        mismatch = find.mismatch(text1)
+        if mismatch == "Success":
+            print("Success")
+        else:
+            print(mismatch)
+        eleif "I" in text:
+            text1 = input()
+            mismatch = find_mismatch(text1)
+            if mismatch == "Success":
+                print("Success")
+            else:
+                print (mismatch)
+
+
+if __name__ == "__main__":
     main()
